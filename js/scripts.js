7e83e33392e940693c1a4f4651ac465171d1424c
Kopano El Nkitseng Matlala <62238987+kenmatlala@users.noreply.github.com>
2023-03-09 16:06:52 +0200

    just a watch html page

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+let image = document.getElementById("image");
+
+image.onmouseover = function() {
+  image.classList.add("wobble");
+};
+
+image.onmouseout = function() {
+  image.classList.remove("wobble");
+};
